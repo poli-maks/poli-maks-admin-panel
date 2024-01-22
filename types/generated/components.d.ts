80e3a59a99960ea34bb9bb1cfcdedad3325b762a
GoodPointt <dev6012@gmail.com>
2024-01-22 19:03:10 +0200

    fix: edit Product model

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -5,12 +5,17 @@ export interface TableTable extends Schema.Component {
   info: {
     displayName: 'Table';
     icon: 'apps';
+    description: '';
   };
   attributes: {
     article: Attribute.String;
     diameter: Attribute.String;
     length: Attribute.String;
     weight: Attribute.String;
+    volume: Attribute.String;
+    wall_thickness: Attribute.String;
+    coating_thickness: Attribute.String;
+    cross_section: Attribute.String;
   };
 }
 
