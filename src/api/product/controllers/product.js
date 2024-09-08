04f1ee2767fb9a6cefd93274296aba21d9087b5a
poli-maks <156600641+poli-maks@users.noreply.github.com>
2024-09-08 22:33:35 +0300

    url update

--- src/api/product/controllers/product.js
+++ src/api/product/controllers/product.js
@@ -6,4 +6,17 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::product.product');
+module.exports = createCoreController('api::product.product', ({ strapi }) => ({
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+
+    // Use the slug to find the product
+    const entity = await strapi.db.query('api::product.product').findOne({
+      where: { slug },
+    });
+
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(sanitizedEntity);
+  },
+}));
