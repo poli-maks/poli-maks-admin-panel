e492a5fbfbd5ad121af7a8b6e1facf5eec78585e
poli-maks <156600641+poli-maks@users.noreply.github.com>
2024-09-09 00:07:56 +0300

    update

--- config/cron-tasks.js
+++ config/cron-tasks.js
@@ -1,18 +1,19 @@
 const { STRAPI_URL } = process.env;
 const axios = require("axios");
 
-module.exports = {
-  myJob: {
-    task: async ({ strapi }) => {
-      try {
-        const response = await axios.get(STRAPI_URL + "/api/cases");
-        if (response) console.log("🐇 Wake up, Neo...");
-      } catch (error) {
-        console.error("❌ Waking up Error!", error);
-      }
-    },
-    options: {
-      rule: "*/14 * * * *",
-    },
-  },
-};
+// Comment out or remove this job
+// module.exports = {
+//   myJob: {
+//     task: async ({ strapi }) => {
+//       try {
+//         const response = await axios.get(STRAPI_URL + "/api/cases");
+//         if (response) console.log("🐇 Wake up, Neo...");
+//       } catch (error) {
+//         console.error("❌ Waking up Error!", error);
+//       }
+//     },
+//     options: {
+//       rule: "*/14 * * * *",
+//     },
+//   },
+// };
