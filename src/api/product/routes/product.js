cd2136b05f88b054cdc8df81c627162b7647e94c
poli-maks <156600641+poli-maks@users.noreply.github.com>
2024-09-08 22:37:46 +0300

    url update

--- src/api/product/routes/product.js
+++ src/api/product/routes/product.js
@@ -6,4 +6,16 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::product.product');
+module.exports = createCoreRouter('api::product.product', {
+  config: {
+    findOne: {
+      // Define a custom route for fetching by slug
+      method: 'GET',
+      path: '/products/:slug',
+      handler: 'product.findOne',
+      config: {
+        auth: false, // Set to true if you need authentication
+      },
+    },
+  },
+});
